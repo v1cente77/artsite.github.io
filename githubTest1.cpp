aad9ce8ba649c0a1bfa64e1f04e06c1372b48bcc
v1cente77 <vicente.amigo.77@gmail.com>
2023-10-13 22:19:23 +0400

    viva com

--- githubTest1.cpp
+++ githubTest1.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello World!";   // comment 2
-    std::cout << "viva forever";
+    std::cout << "viva forever";   // comment on viva
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
